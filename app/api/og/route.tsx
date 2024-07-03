4dfb89e882a975b5d8ffac2fd71883321895bbce
bilesimomenegali <jbmenegali@gmail.com>
2024-07-03 19:22:23 -0300

    add last development

---
+++ app/api/og/route.tsx
@@ -0,0 +1,66 @@
+import { NextRequest } from "next/server";
+import { ImageResponse } from "next/og";
+import { siteConfig } from "@/config/site";
+
+export const runtime = "edge";
+
+const interBold = fetch(
+  new URL("../../../assets/fonts/Inter-Bold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+export async function GET(req: NextRequest) {
+  try {
+    const fontBold = await interBold;
+
+    const { searchParams } = req.nextUrl;
+    const title = searchParams.get("title");
+
+    if (!title) {
+      return new Response("No title provided", { status: 500 });
+    }
+
+    const heading = title.length > 140 ? `${title.substring(0, 140)}...` : title;
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#fff',
+            fontSize: 32,
+            fontWeight: 600,
+          }}
+        >
+          <svg
+            width="75"
+            viewBox="0 0 75 65"
+            fill="#000"
+            style={{ margin: '0 75px' }}
+          >
+            <path d="M37.59.25l36.95 64H.64l36.95-64z"></path>
+          </svg>
+          <div style={{ marginTop: 40 }}>{heading}</div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: "Inter",
+            data: fontBold,
+            style: "normal",
+            weight: 700,
+          },
+        ],
+      }
+    );
+  } catch (error) {
+    return new Response("Failed to generate image", { status: 500 });
+  }
+}
--- config/site.ts
+++ config/site.ts
@@ -2,6 +2,7 @@ export const siteConfig = {
   name: 'MyFirstBlog',
   url: 'https://myfirstblog.com',
   description: 'My first blog, using Nextjs14, velite, tailwind and schadcn',
+  author: "Jhon Doe",
   links: {
     twitter: 'https://twitter.com/bilesimo_',
     github: 'https://github.com/bilesimomenegali',
